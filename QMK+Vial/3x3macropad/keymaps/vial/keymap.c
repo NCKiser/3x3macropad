246d90137bed7469e2e3f618de1541602669f328
NCKiser <noahkiser95mygc@gmail.com>
2021-08-03 04:33:19 -0400

    Tweaked Vial Firmware

--- QMK+Vial/3x3macropad/keymaps/vial/keymap.c
+++ QMK+Vial/3x3macropad/keymaps/vial/keymap.c
@@ -7,20 +7,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_4,       KC_5,      KC_6,       \
   KC_7,       KC_8,      LT(1, KC_9) \
 ),
-
 [1] = LAYOUT( \
   KC_ESC,   KC_PLUS, KC_MINS, \
   KC_ENTER, KC_ASTR, KC_SLSH, \
   KC_0,     KC_DOT,  KC_TRNS  \
 ),
-
 [2] = LAYOUT( \
   KC_TRNS, KC_TRNS, KC_TRNS, \
   KC_TRNS, KC_TRNS, KC_TRNS, \
   KC_TRNS, KC_TRNS, KC_TRNS  \
 ),
-
 [3] = LAYOUT( \
+  KC_TRNS, KC_TRNS, KC_TRNS, \
+  KC_TRNS, KC_TRNS, KC_TRNS, \
+  KC_TRNS, KC_TRNS, KC_TRNS  \
+),
+[4] = LAYOUT( \
+  KC_TRNS, KC_TRNS, KC_TRNS, \
+  KC_TRNS, KC_TRNS, KC_TRNS, \
+  KC_TRNS, KC_TRNS, KC_TRNS  \
+),
+[5] = LAYOUT( \
   KC_TRNS, KC_TRNS, KC_TRNS, \
   KC_TRNS, KC_TRNS, KC_TRNS, \
   KC_TRNS, KC_TRNS, KC_TRNS  \
